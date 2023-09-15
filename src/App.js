3764215f1ac13b4f91274ab704097375bb179ccc
artem_epelman <ded.kropiva@gmail.com>
2023-09-15 16:07:14 +0300

    redux_base_lesson

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
-  return (
-    <div >
 
-    </div>
-  );
+
+    const dispatch = useDispatch()
+
+    const store = useSelector(state => state)
+
+    const increment = () => {
+        dispatch({type:'INC'})
+    }
+
+    const decrement = () => {
+        dispatch({type:'DEC'})
+    }
+
+
+    const res = () => {
+        dispatch({type:'RES'})
+    }
+
+
+    return (
+        <div>
+
+            <h1>{store}</h1>
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
+            <button onClick={res}>reset</button>
+
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+
+const baseReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return ++state
+        case 'DEC':
+            return --state
+        case 'RES':
+            return 0
+        default:
+            return state
+    }
+    return state
+}
+
+
+const store = createStore(baseReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
