fb2c0ea28a0aca6c2b7bfc04eede7ccc804ca82c
artem_epelman <ded.kropiva@gmail.com>
2023-09-15 16:09:58 +0300

    redux_toolkit_lesson_initial

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import {useDispatch, useSelector} from "react-redux";
 
 function App() {
 
 
-    const dispatch = useDispatch()
-
-    const store = useSelector(state => state)
-
-    const increment = () => {
-        dispatch({type:'INC'})
-    }
-
-    const decrement = () => {
-        dispatch({type:'DEC'})
-    }
-
-
-    const res = () => {
-        dispatch({type:'RES'})
-    }
-
-
     return (
-        <div>
+    <>
 
-            <h1>{store}</h1>
-            <button onClick={increment}>increment</button>
-            <button onClick={decrement}>decrement</button>
-            <button onClick={res}>reset</button>
-
-        </div>
-    );
+    </>
+    )
 }
 
 export default App;
